d122c3cdce37fd3d392c8731b2e8993b1eb1cba3
Tijs Verkoyen <tijs@verkoyen.eu>
2013-02-13 13:56:41 +0100

    Added tests

---
+++ tests/RecreatexTest.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once '../../../autoload.php';
+require_once 'config.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+
+use \TijsVerkoyen\Recreatex\Recreatex;
+
+/**
+ * test case.
+ */
+class RecreatexTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Recreatex instance
+     *
+     * @var	Recreatex
+     */
+    private $recreatex;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->recreatex = new Recreatex(SERVER, PORT);
+	    $this->recreatex->createServiceContext(SHOP_ID, DIVISION_ID, LANGUAGE);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        $this->recreatex = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Tests Recreatex->getTimeOut()
+     */
+    public function testGetTimeOut()
+    {
+        $this->recreatex->setTimeOut(5);
+        $this->assertEquals(5, $this->recreatex->getTimeOut());
+    }
+
+    /**
+     * Tests Recreatex->getUserAgent()
+     */
+    public function testGetUserAgent()
+    {
+        $this->recreatex->setUserAgent('testing/1.0.0');
+        $this->assertEquals(
+            'PHP Recreatex/' . Recreatex::VERSION . ' testing/1.0.0',
+            $this->recreatex->getUserAgent()
+        );
+    }
+}
